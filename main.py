4ad6c56fb7c4cdcadde7513da4f4cd0ef0e7c070
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-10-27 22:17:14 +0200

    Zet de bericht in de database in plaats van .csv

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 import random
 import csv
+import psycopg2
+
+connection_string = "host='localhost' dbname='zuil' user='postgres' password='Guus2005!'"
+STATIONS = ["Hilversum", "Bussum-Zuid", "Naarden-Bussum"]
 
 def main():
     mode = eval(input("Station Zeil programma\n\nModes:\n1. Klant\n2. Moderator\nKeuze: "))
@@ -10,33 +14,33 @@ def main():
         Moderator()
 
 def Reiziger(naam = ""):
-    berichtInfo = {}
     
     if naam == "":
         naam = input("Naam (Leeg betekent anoniem): ")
         if naam == "": naam = "Anoniem"
-    berichtInfo["naam"] = naam
     
     bericht = input("Het bericht (Max 140 karakters): \n")
     if len(bericht) >= 140:
         print(f"Bericht is the lang: {len(bericht)} karakters lang, maar het mag maar 140 karakters lang zijn")
         Reiziger(naam)
-    berichtInfo["bericht"] = bericht
     
-    tijd = datetime.now()
-    berichtInfo["datum"] = tijd.strftime("%d-%m-%Y")
-    berichtInfo["tijd"] = tijd.strftime("%X")
+    datumtijd = datetime.now()
     
-    with open("Zuil\stations.txt") as file:
-        stations = file.readlines()
-        station = random.choice(stations)
-        berichtInfo["station"] = station.strip()
+    station = random.choice(STATIONS)
 
     commit = input("Wilt u het bericht versturen? (y/n)\n")
+    
     if commit == "y":
-        with open("Zuil\\berichten.csv", "a", newline='') as file:
-            writer = csv.DictWriter(file, berichtInfo.keys())
-            writer.writerow(berichtInfo)
+        conn = psycopg2.connect(connection_string)
+        cursor = conn.cursor()
+        query = """ INSERT INTO bericht(bericht, datumtijd, naamreiziger, station)
+                    VALUES
+                        (%s, %s, %s, %s);
+                """ 
+        data = (bericht, datumtijd, naam, station)
+        cursor.execute(query, data)
+        conn.commit()
+        conn.close()
 
 def Moderator():
     naam = input("Naam: ")
