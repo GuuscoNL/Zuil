3468d2af214e078101802d52718934ff0ff43638
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-11-07 00:47:31 +0100

    Afronding moderatie

--- main.py
+++ main.py
@@ -116,23 +116,27 @@ def Moderator():
     cursor.execute(query, data)
     conn.commit()
 
-def krijg_berichten(station):
-    
+def krijg_alle_berichten():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    query = """ select * from bericht 
-                WHERE station = %s
-                ORDER BY datumtijd DESC
-                LIMIT 5;"""
-    data = (station,)
-    cursor.execute(query, data)
+    query = """ SELECT 	R.id,
+                        R.naamreiziger,
+                        R.station,
+                        R.datumtijd,
+                        R.bericht,
+                        L.goedgekeurd
+                FROM beoordeling AS L
+	                RIGHT JOIN bericht AS R ON L.berichtid = R.id
+                ORDER BY datumtijd DESC;"""
+    cursor.execute(query)
     return cursor.fetchall()
 
-def maak_bericht_compact(text):
+def maak_bericht_compact(text, max_char, start_woord=""):
     woorden = text.split(" ")
+    woorden.insert(0, start_woord)
     sublines = [""]
     index = 0
     for woord in woorden:
-        if len(sublines[index]) + len(woord) >= 35:
+        if len(sublines[index]) + len(woord) >= max_char:
             index += 1
             sublines.append(woord + " ")
         else:
@@ -149,8 +153,28 @@ def krijg_faciliteiten(station):
     cursor.execute(query, data)
     return cursor.fetchall()
 
+def mod_login(entry_naam, entry_email, label_info):
+    naam = entry_naam.get()
+    
+    email = entry_email.get()
+    
+    # Kijk of de moderator al bestaat
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = """ SELECT id FROM moderator
+                WHERE naam = %s AND email = %s"""
+    data = (naam, email)
+    cursor.execute(query, data)
+    mod = None
+    for record in cursor.fetchall():
+        mod = record
 
-
+    if mod is None:
+        label_info["text"] = "Mod account bestaat niet, probeer opnieuw"
+    else:
+        global cur_mod
+        cur_mod = mod
+        modInlog_change_to_mod()
+        
 
 # ---------- GUI ----------
 
@@ -181,12 +205,95 @@ def reiziger_change_to_reiziger_einde():
 def stations_change_to_menu():
     frame_menu.pack(fill="both", expand=1)
     frame_stations.forget()
+    root.title("Menu")
 
 def top_exit(top): 
     global schermIsOpen
     schermIsOpen = False
     top.destroy()
+    
+def menu_change_to_modInlog():
+    frame_modInlog.pack(fill="both", expand=1)
+    
+    frame_menu.forget()
+    root.title("Moderator login")
 
+def modInlog_change_to_menu(entry_naam, entry_email):
+    frame_menu.pack(fill="both", expand=1)
+    
+    entry_naam.delete(0, "end")
+    entry_email.delete(0, "end")
+    
+    frame_modInlog.forget()
+    root.title("Menu")
+    
+def modInlog_change_to_mod():
+    frame_mod.pack(fill="both", expand=1)
+    
+    frame_modInlog.forget()
+    root.title("Moderator")
+    
+def mod_change_to_menu():
+    frame_menu.pack(fill="both", expand=1)
+    frame_mod.forget()
+    root.title("Menu")
+
+def listbox_berichten_selected(label_naam, label_station, label_datum, label_bericht, label_goedkeuring, frame_keuringButtons, event):
+    for index in listbox_berichten.curselection():
+        bericht = berichten_lijst[index]
+        label_naam["text"] = f"Naam: {bericht['naamreiziger']}"
+        label_station["text"] = f"Station: {bericht['station']}"
+        label_datum["text"] = f"Datum: {bericht['datumtijd'].strftime('%Y-%m-%d %H:%M:%S')}"
+        label_bericht["text"] = maak_bericht_compact(bericht['bericht'], 35, "Bericht:")
+        
+        if bericht['goedgekeurd']:
+            goedkeuring = "Goedgekeurd"
+            frame_keuringButtons.forget()
+            
+        elif bericht['goedgekeurd'] == False:
+            goedkeuring = "Niet goedgekeurd"
+            frame_keuringButtons.forget()
+        else:
+            goedkeuring = "Nog niet beoordeeld"
+            frame_keuringButtons.pack(pady=5,anchor="w",
+                       side="left")
+        
+        label_goedkeuring["text"] = f"Beoordeling: {goedkeuring}"
+
+def keur_bericht(keuring, label_goedkeuring):
+    for index in listbox_berichten.curselection():
+        bericht = berichten_lijst[index]
+        cursor = conn.cursor()
+        query = """ INSERT INTO beoordeling(goedgekeurd, datumtijd, berichtid, moderatorid)
+                        VALUES
+                            (%s, %s, %s, %s);"""
+        data = (keuring, datetime.now(), bericht['id'], cur_mod['id'])
+        cursor.execute(query, data)
+        conn.commit()
+        if keuring:
+            goedkeuring = "Goedgekeurd"
+        else:
+            goedkeuring = "Niet goedgekeurd"
+        label_goedkeuring["text"] = f"Beoordeling: {goedkeuring}"
+        frame_keuringButtons.forget()
+        
+        update_berichten()
+        
+def update_berichten():
+    listbox_berichten.delete(0, "end")
+    berichten = krijg_alle_berichten()
+    global berichten_lijst
+    berichten_lijst = []
+    for i in range(len(berichten)):
+        bericht = berichten[i]
+        listbox_berichten.insert(i, f"Bericht van: {bericht['naamreiziger']}")
+        
+        if bericht['goedgekeurd'] == True:
+            listbox_berichten.itemconfig(i, background="#63ff63")
+        elif bericht['goedgekeurd'] == False:
+            listbox_berichten.itemconfig(i, background= "#ff7a7a")
+            
+        berichten_lijst.append(bericht)
 
 # Initialise app
 winWidth = 400
@@ -218,7 +325,8 @@ def load_menu():
 
     button_moderator = Button(master=frame_menu, 
                             text="Moderator",
-                            cursor="hand2")
+                            cursor="hand2",
+                            command=menu_change_to_modInlog)
     button_moderator.pack(pady=5)
 
     button_scherm = Button(master=frame_menu, 
@@ -335,60 +443,62 @@ def open_zuilscherm(station):
     top.geometry("500x400")
     root.resizable(False, False)
     
-    label = Label(master=top, text=f"Welkom bij station {station}", bg="#ffcc17", font= font_title)
-    label.pack(pady=10)
+    label_title = Label(master=top, text=f"Welkom bij station {station}", bg="#ffcc17", font= font_title)
+    label_title.pack(pady=10)
     
     # ---------- Berichten ----------
     
     frame_bericht = Frame(master=top, bg="#ffcc17")
     frame_bericht.pack(fill="both", expand=1)
 
-    berichten = krijg_berichten(station)
+    berichten = krijg_alle_berichten()
     
     for bericht in berichten:
+        if bericht["goedgekeurd"] == False or bericht["goedgekeurd"] is None: continue
+        if bericht["station"] != station: continue
         bericht_text = bericht["bericht"]
         
-        bericht_text = maak_bericht_compact(bericht_text)
+        bericht_text = maak_bericht_compact(bericht_text, 35)
         
         sub_frame_bericht = LabelFrame(master=frame_bericht, bg="#ffcc17", text=f"{bericht['naamreiziger']} zei: ")
         sub_frame_bericht.pack(pady=3)
         
-        label = Label(master=sub_frame_bericht, text=bericht_text, font=font_bericht, bg="#ffcc17")
-        label.pack()
+        label_title = Label(master=sub_frame_bericht, text=bericht_text, font=font_bericht, bg="#ffcc17")
+        label_title.pack()
     
     # ---------- Iconen ----------
     faciliteiten = krijg_faciliteiten(station)
     
     frame_iconen = Frame(master=top, bg="#ffcc17")
-    frame_iconen.pack(anchor="s", side="left")
+    frame_iconen.pack(anchor="s", side="left", padx=2, pady=3)
     
     if faciliteiten[0]["ov_bike"]:
         icon = PhotoImage(file="Zuil\img_faciliteiten\img_ovfiets.png")
         icon = icon.subsample(2)
-        label = Label(master=frame_iconen, image=icon)
-        label.photo = icon
-        label.pack(side="left", padx=2)
+        label_title = Label(master=frame_iconen, image=icon)
+        label_title.photo = icon
+        label_title.pack(side="left", padx=2)
         
     if faciliteiten[0]["elevator"]:
         icon = PhotoImage(file="Zuil\img_faciliteiten\img_lift.png")
         icon = icon.subsample(2)
-        label = Label(master=frame_iconen, image=icon)
-        label.photo = icon
-        label.pack(side="left", padx=2)
+        label_title = Label(master=frame_iconen, image=icon)
+        label_title.photo = icon
+        label_title.pack(side="left", padx=2)
         
     if faciliteiten[0]["toilet"]:
         icon = PhotoImage(file="Zuil\img_faciliteiten\img_toilet.png")
         icon = icon.subsample(2)
-        label = Label(master=frame_iconen, image=icon)
-        label.photo = icon
-        label.pack(side="left", padx=2)
+        label_title = Label(master=frame_iconen, image=icon)
+        label_title.photo = icon
+        label_title.pack(side="left", padx=2)
         
     if faciliteiten[0]["park_and_ride"]:
         icon = PhotoImage(file="Zuil\img_faciliteiten\img_pr.png")
         icon = icon.subsample(2)
-        label = Label(master=frame_iconen, image=icon)
-        label.photo = icon
-        label.pack(side="left", padx=2)
+        label_title = Label(master=frame_iconen, image=icon)
+        label_title.photo = icon
+        label_title.pack(side="left", padx=2)
     
     # ---------- Weather ----------
     
@@ -401,7 +511,7 @@ def open_zuilscherm(station):
     temp = w.temperature('celsius')
     
     frame_weer = LabelFrame(master=top, bg="#ffcc17", text="Weer")
-    frame_weer.pack(anchor="s", side="right")
+    frame_weer.pack(anchor="s", side="right", padx=2, pady=2)
     
     text_weer = f"""Temp: {temp["temp"]:7} C
 Gevoel: {temp["feels_like"]:5} C
@@ -415,11 +525,158 @@ Min: {temp["temp_min"]:9} C"""
 
     schermIsOpen = True
 
+def load_modInlog():
+    global frame_modInlog
+    frame_modInlog = Frame(root)
+    frame_modInlog.pack(fill="both", expand=1)
+
+    label_title = Label(master=frame_modInlog, 
+                text="Log in met je naam en email",)
+    label_title.pack(pady=5)
+
+    sub_frame_naam = Frame(frame_modInlog)
+    sub_frame_naam.pack(anchor="n", side="top")
+
+    label_naam = Label(sub_frame_naam, text="Naam: ")
+    label_naam.pack(side="left")
+
+    entry_naam = Entry(sub_frame_naam, width=15)
+    entry_naam.pack(pady=5, side="left")
+
+    sub_frame_bericht = Frame(frame_modInlog)
+    sub_frame_bericht.pack(anchor="n", side="top")
+
+    label_bericht = Label(sub_frame_bericht, text="email: ")
+    label_bericht.pack(side="left")
+    
+    label_info = Label(frame_modInlog, text="")
+
+    entry_email = Entry(sub_frame_bericht, width=27)
+    entry_email.pack(pady=5, side="left")
+    button_submit = Button(frame_modInlog, 
+                        text="Log in",
+                        cursor="hand2",
+                        command=partial(mod_login, entry_naam, entry_email, label_info))
+    button_submit.pack(pady=5)
+
+    
+    label_info.pack()
+    
+    
+    sub_frame_backbutton = Frame(frame_modInlog)
+    sub_frame_backbutton.pack(anchor="s", side="left")
+
+    button_back = Button(master=sub_frame_backbutton, 
+                        text="back",
+                        cursor="hand2",
+                        command=partial(modInlog_change_to_menu, entry_naam, entry_email))
+    button_back.pack(anchor="s", side="left")
+    
+    frame_modInlog.forget()
+
+
+def load_mod():
+    global frame_mod
+    frame_mod = Frame(root)
+    frame_mod.pack(fill="both", expand=1)
+    
+    label = Label(master=frame_mod, 
+                  text="Berichten")
+    label.pack()
+    
+    frame_listbox = Frame(master=frame_mod)
+    frame_listbox.pack(anchor="w", side="left")
+    
+    scrollbar = Scrollbar(frame_listbox, orient=VERTICAL)
+    
+    
+    global listbox_berichten
+    listbox_berichten = Listbox(master=frame_listbox, 
+                                height = 10, 
+                                width = 30,
+                                yscrollcommand=scrollbar.set)
+
+    listbox_berichten.pack(anchor="w", side="left")
+    
+    scrollbar.config(command=listbox_berichten.yview)
+    scrollbar.pack(side="left", fill=Y)
+    
+    
+    frame_bericht = LabelFrame(master=frame_mod,
+                               text="Bericht")
+    frame_bericht.pack(anchor="w",
+                       side="left")
+    
+    label_naam = Label(master=frame_bericht,
+                       text="Naam:")
+    label_naam.pack(anchor="w")
+    
+    label_station = Label(master=frame_bericht,
+                          text="Station:")
+    label_station.pack(anchor="w")
+    
+    label_datum = Label(master=frame_bericht,
+                       text="Datum:")
+    label_datum.pack(anchor="w")
+    
+    label_bericht = Label(master=frame_bericht,
+                          text="Bericht:")
+    label_bericht.pack(anchor="w")
+    
+    label_goedkeuring = Label(master=frame_bericht,
+                          text="Goedkeuring: ")
+    label_goedkeuring.pack(anchor="w")
+    
+    global frame_keuringButtons
+    frame_keuringButtons = Frame(master=frame_bericht)
+    frame_keuringButtons.pack(pady=5,anchor="w",
+                       side="left")
+    
+    button_keurgoed = Button(master=frame_keuringButtons,
+                             text="Keur goed",
+                             command=partial(keur_bericht, True, label_goedkeuring))
+    button_keurgoed.pack(padx=5,
+                         anchor="w",
+                         side="left")
+    
+    button_keuraf = Button(master=frame_keuringButtons,
+                             text="Keur niet goed",
+                             command=partial(keur_bericht, False, label_goedkeuring))
+    button_keuraf.pack(padx=5,
+                       anchor="w",
+                       side="left")
+    
+    frame_keuringButtons.forget()
+    
+    
+    listbox_berichten.bind("<<ListboxSelect>>", partial(listbox_berichten_selected, 
+                                                        label_naam, label_station, 
+                                                        label_datum, label_bericht, 
+                                                        label_goedkeuring, 
+                                                        frame_keuringButtons))
+    
+    update_berichten()
+    
+    sub_frame_backbutton = Frame(frame_mod)
+    sub_frame_backbutton.place(x=3, y=winHeight-27)
+
+    button_back = Button(master=sub_frame_backbutton, 
+                        text="back",
+                        cursor="hand2",
+                        command=mod_change_to_menu)
+    button_back.pack(anchor="s", side="left")
+    
+    
+    frame_mod.forget()
+
+
 # Laad alle frames
 load_menu()
 load_reiziger()
 load_reiziger_einde()
 load_stations()
+load_modInlog()
+load_mod()
 
     
 root.mainloop()
