8a09454247ff9f19764eb8524b9465a203944770
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-11-07 16:08:19 +0100

    Voeg logo toe

--- main.py
+++ main.py
@@ -218,11 +218,11 @@ def menu_change_to_modInlog():
     page_menu.forget()
     root.title("Moderator login")
 
-def modInlog_change_to_menu(entry_naam, entry_email):
+def modInlog_change_to_menu():
     page_menu.pack(fill="both", expand=1)
     
-    entry_naam.delete(0, "end")
-    entry_email.delete(0, "end")
+    entry_mod_naam.delete(0, "end")
+    entry_mod_email.delete(0, "end")
     
     page_modInlog.forget()
     root.title("Menu")
@@ -235,6 +235,10 @@ def modInlog_change_to_mod():
     
 def mod_change_to_menu():
     page_menu.pack(fill="both", expand=1)
+    
+    entry_mod_naam.delete(0, "end")
+    entry_mod_email.delete(0, "end")
+    
     page_mod.forget()
     root.title("Menu")
 
@@ -304,6 +308,7 @@ root.title("Menu")
 #root.eval("tk::PlaceWindow . center")
 root.geometry(f"{winWidth}x{winHeight}")
 root.resizable(False, False)
+root.iconbitmap("Zuil/images/NS-logo.ico")
 
 # Menu Frame
 def load_menu():
@@ -441,7 +446,8 @@ def open_zuilscherm(station):
     global top
     top = Toplevel(bg="#ffcc17")
     top.geometry("500x400")
-    root.resizable(False, False)
+    top.iconbitmap("Zuil/images/NS-logo.ico")
+    top.resizable(False, False)
     
     label_title = Label(master=top, text=f"Welkom bij station {station}", bg="#ffcc17", font= font_title)
     label_title.pack(pady=10)
@@ -473,33 +479,33 @@ def open_zuilscherm(station):
     frame_iconen.pack(anchor="s", side="left", padx=2, pady=3)
     
     if faciliteiten[0]["ov_bike"]:
-        icon = PhotoImage(file="Zuil\img_faciliteiten\img_ovfiets.png")
+        icon = PhotoImage(file="Zuil\images\img_ovfiets.png")
         icon = icon.subsample(2)
         label_title = Label(master=frame_iconen, image=icon)
         label_title.photo = icon
         label_title.pack(side="left", padx=2)
         
     if faciliteiten[0]["elevator"]:
-        icon = PhotoImage(file="Zuil\img_faciliteiten\img_lift.png")
+        icon = PhotoImage(file="Zuil\images\img_lift.png")
         icon = icon.subsample(2)
         label_title = Label(master=frame_iconen, image=icon)
         label_title.photo = icon
         label_title.pack(side="left", padx=2)
         
     if faciliteiten[0]["toilet"]:
-        icon = PhotoImage(file="Zuil\img_faciliteiten\img_toilet.png")
+        icon = PhotoImage(file="Zuil\images\img_toilet.png")
         icon = icon.subsample(2)
         label_title = Label(master=frame_iconen, image=icon)
         label_title.photo = icon
         label_title.pack(side="left", padx=2)
         
     if faciliteiten[0]["park_and_ride"]:
-        icon = PhotoImage(file="Zuil\img_faciliteiten\img_pr.png")
+        icon = PhotoImage(file="Zuil\images\img_pr.png")
         icon = icon.subsample(2)
         label_title = Label(master=frame_iconen, image=icon)
         label_title.photo = icon
         label_title.pack(side="left", padx=2)
-    
+
     # ---------- Weather ----------
     
     owm = pyowm.OWM("6c9174d05f353b6f3fba0d4c53cb4728")
@@ -540,8 +546,9 @@ def load_modInlog():
     label_naam = Label(sub_frame_naam, text="Naam: ")
     label_naam.pack(side="left")
 
-    entry_naam = Entry(sub_frame_naam, width=15)
-    entry_naam.pack(pady=5, side="left")
+    global entry_mod_naam
+    entry_mod_naam = Entry(sub_frame_naam, width=15)
+    entry_mod_naam.pack(pady=5, side="left")
 
     sub_frame_bericht = Frame(page_modInlog)
     sub_frame_bericht.pack(anchor="n", side="top")
@@ -551,12 +558,13 @@ def load_modInlog():
     
     label_info = Label(page_modInlog, text="")
 
-    entry_email = Entry(sub_frame_bericht, width=27)
-    entry_email.pack(pady=5, side="left")
+    global entry_mod_email
+    entry_mod_email = Entry(sub_frame_bericht, width=27)
+    entry_mod_email.pack(pady=5, side="left")
     button_submit = Button(page_modInlog, 
                         text="Log in",
                         cursor="hand2",
-                        command=partial(mod_login, entry_naam, entry_email, label_info))
+                        command=partial(mod_login, entry_mod_naam, entry_mod_email, label_info))
     button_submit.pack(pady=5)
 
     
@@ -569,11 +577,10 @@ def load_modInlog():
     button_back = Button(master=sub_frame_backbutton, 
                         text="back",
                         cursor="hand2",
-                        command=partial(modInlog_change_to_menu, entry_naam, entry_email))
+                        command=modInlog_change_to_menu)
     button_back.pack(anchor="s", side="left")
     
     page_modInlog.forget()
-
 
 def load_mod():
     global page_mod
